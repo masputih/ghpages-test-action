34eb1981e3876215a30cc22e11d0dd27b344b9a5
Anggie Bratadinata <anggie.bratadinata@gmail.com>
2024-10-05 11:23:18 +0700

    Initial commit

---
+++ prettier.config.js
@@ -0,0 +1,13 @@
+// @prefix prettiercfg
+// @description
+
+// prettier.config.js
+export default {
+  bracketSpacing: true,
+  semi: true,
+  trailingComma: 'all',
+  printWidth: 120,
+  tabWidth: 2,
+  singleQuote: true,
+  plugins: [],
+};
